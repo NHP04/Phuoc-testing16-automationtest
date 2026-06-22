d19837610de4fbd38a7a31ce0c5e9bc1722b9f34
Phuoc <Phuocnguyen10112004@gmail.com>
2026-06-23 00:53:23 +0700

    push code java co ban

---
+++ src/buoi14/Main.java
@@ -0,0 +1,109 @@
+package buoi14;
+
+import java.util.Scanner;
+
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Mời bàn nhập số: ");
+//        int number = scanner.nextInt();
+
+//          //bài 1: ktr âm dương
+//        if (number >=0 ){
+//            System.out.println("số bạn nhập là số dương");
+//        }
+//        else {
+//            System.out.println("số bạn nhập là số âm");
+//        }
+//
+//
+//          //bài 2: ktr chẵn lẻ
+//        if (number %2 ==0 ){
+//            System.out.println("số bạn nhập là số chẵn");
+//        }
+//        else {
+//            System.out.println("số bạn nhập là số lẻ");
+//        }
+
+
+//          //bài 2: ktr diem 9 - 10: xuất sắc, 8- <9: giỏi, 6.5- <8: khá, 5- <6.5: TB.
+//        System.out.println("Mời bạn nhập điểm học sinh:");
+//        float number = scanner.nextFloat();
+
+//        if (number <= 0 || number > 10) {
+//            System.out.println("Điểm bạn nhập không hợp lệ");
+//        } else if (number >= 9 && number <= 10) {
+//            System.out.println("Xuất sắc");
+//        } else if (number >= 8 && number < 9) {
+//            System.out.println("Giỏi");
+//        } else if (number >= 6.5 && number < 8) {
+//            System.out.println("Khá");
+//        } else if (number >= 5 && number < 6.5) {
+//            System.out.println("Trung bình");
+//        } else {
+//            System.out.println("Yếu");
+//        }
+
+//        bai 4: tinh tien ve thee do tuoi va loai ve
+//        input
+//        tuoi, loai ve
+//        tre em < 12 tuoi mien phi
+//        nguoi cao tuoi (>=60 tuoi): 50.000 (cho tat ca loai ve)
+//        nguoi lon con lai:
+//        VIP: 200.000
+//        thuong: 150.000
+//        case 1: 11, VIP => free
+//        case 2: 65, VIP => 50.000
+//        case 3: 20, VIP => 200.000
+//        case 4: 20, thuong => 150.000
+//
+//        System.out.println("Mời bạn nhập tuổi:");
+//        int tuoi = scanner.nextInt();
+//        scanner.nextLine();
+//
+//        System.out.println("Mời bạn nhập vé (Vip/thuong):");
+//        String ve = scanner.nextLine();
+//
+//        if (tuoi <12){
+//            System.out.println("trẻ em được miễn phí");
+//        } else if (tuoi >= 60) {
+//            System.out.println("giá vé là 50.000");
+//        }else{
+//            if (ve.equalsIgnoreCase("VIP")){
+//                System.out.println("giá vé là 200.000");
+//            }
+//            else{
+//                System.out.println("giá vé là 150.000");
+//            }
+//        }
+
+
+        //Bai 5: tìm số lớn nhất trong 3 số
+
+//        System.out.println("nhập a:");
+//        int a = scanner.nextInt();
+//        System.out.println("nhập b:");
+//        int b = scanner.nextInt();
+//        System.out.println("nhập c:");
+//        int c = scanner.nextInt();
+//
+//        if (a >= c && a >= b){
+//            System.out.println("số lớn nhất là " + a);
+//        } else if (b >= a && b >= c){
+//            System.out.println("số lớn nhất là " + b);
+//        }else if (c >= a && c >= b) {
+//            System.out.println("số lớn nhất là " + c);
+//        }else{
+//            System.out.println("ba số bằng nhau");
+//        }
+
+
+        //Bai 1 vong lap
+
+
+
+        }
+    }
